6fe1b85d1cae12f7a92eacc913eff383951b3a4c
Dhruvisha <dhruvibhaliya01@gmail.com>
2025-09-07 22:27:28 +0530

    Add Second Web Project

---
+++ SessionWeb2App/src/main/java/Servlets/LoginServlet.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package Servlets;
+
+import java.io.*;
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.*;
+
+/**
+ *
+ * @author DELL
+ */
+@WebServlet(name = "LoginServlet", urlPatterns = {"/LoginServlet"})
+public class LoginServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+
+        String username = "";
+        String password = "";
+        Cookie[] cookies = request.getCookies();
+
+        if (cookies != null) {
+            for (int i = 0; i < cookies.length; i++) {
+                Cookie thisCookie = cookies[i];
+
+                if (thisCookie.getName().equals("ValidUser")) {
+                    username = thisCookie.getValue();
+                } else if (thisCookie.getName().equals("ValidPassword")) {
+                    password = thisCookie.getValue();
+                }
+            }
+        }
+
+            out.println("<HTML><HEAD><TITLE>Login Form</TITLE></HEAD>");
+            out.println("<BODY><FORM ACTION='/SessionWeb2App' METHOD='post' NAME='formLogin'>");
+//            out.println("<TABLE><TR><TD ALIGN='center'><IMG SRC='/images/login_security.jpeg' width='64' HEIGHT='64' BORDER='0'>");
+            out.println("<TABLE WIDTH='25%' BORDER='1' ALIGN='center' CELLPADDING='3' CELLSPACING='1' BORDERCOLOR='#000000'>");
+            out.println("<TR BORDERCOLOR='#92CAEB' BGCOLOR='white'><TD COLSPAN='2'>Member Login</TD></TR>");
+            out.println("<TR BORDERCOLOR='#E6F3FB'><TD ALIGN='right'>Username:</TD><TD>");
+            out.println("<INPUT NAME='txtusername' TYPE='text' TABINDEX='1' VALUE='" + username + "' SIZE='15' MAXLENGTH='15'></TD>");
+            out.println("</TR><TR BORDERCOLOR='#E6F3FB'><TD ALIGN='right'>Password:</TD><TD>");
+            out.println("<INPUT NAME='txtpassword' TYPE='password' TABINDEX='2' VALUE='" + password + "' SIZE='15' MAXLENGTH='15'></TD></TR>");
+            out.println("<TR BORDERCOLOR='#E6F3FB'><TD COLSPAN='2' ALIGN='right'>");
+            out.println("<INPUT NAME='chkrem' TYPE='checkbox' VALUE='REMEMBER' TABINDEX='3'>REMEMBER ME</TD></TR>");
+            out.println("<TR BORDERCOLOR='#E0EEF7'><TD COLSPAN='2' ALIGN='right'>");
+            out.println("<INPUT NAME='submit' TYPE='submit' VALUE='Sign In' TABINDEX='4'></TD></TR>");
+            out.println("</TABLE></TD></TR></TABLE></FORM></BODY></HTML>");
+
+            /* TODO output your page here. You may use following sample code. */
+            out.close();
+        }
+    }   
+
---
+++ SessionWeb2App/src/main/java/Servlets/RememberMeServlet.java
@@ -0,0 +1,91 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package Servlets;
+
+import jakarta.servlet.http.*;
+import java.io.*;
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebServlet;
+import java.sql.*;
+
+/**
+ *
+ * @author DELL
+ */
+@WebServlet(name = "RememberMeServlet", urlPatterns = {"/RememberMeServlet"})
+public class RememberMeServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+
+        Connection con = null;
+        Statement st;
+
+        try {
+            String username = "root";
+            String password = "";
+            String url = "jdbc:mysql://localhost/test?useSSL=false";
+
+            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+            con = DriverManager.getConnection(url, username, password);
+
+            if (con != null) {
+                out.println("<BR>Successfully connected to MySQL servler using TCP/IP...<BR>");
+                st = con.createStatement();
+
+                String query = "SELECT user FROM login WHERE user = '" + request.getParameter("txtusername").toString() + "' AND password ='" + request.getParameter("txtpassword").toString() + "'";
+                ResultSet rs = st.executeQuery(query);
+
+                if (rs.next() == false) {
+                    out.println("<BR>Sorry. Invalid Login.Please try again.</BR>");
+                } else {
+                    out.println("<BR>Successful Login.Welcome to the world of Servlets.</BR>");
+                    if (request.getParameter("chkrem") != null) {
+                        Cookie returnLoginUser = new Cookie("ValidUser", request.getParameter("txtusername").toString());
+                        returnLoginUser.setMaxAge(36000);
+                        response.addCookie(returnLoginUser);
+                        Cookie returnLoginPassword = new Cookie("ValidPassword", request.getParameter("txtpassword").toString());
+                        returnLoginPassword.setMaxAge(36000);
+                        response.addCookie(returnLoginPassword);
+                    } else {
+                        Cookie returnLoginUser = new Cookie("ValidUser", "");
+                        returnLoginUser.setMaxAge(0);
+                        response.addCookie(returnLoginUser);
+                        Cookie returnLoginPassword = new Cookie("ValidPassword", "");
+                        returnLoginPassword.setMaxAge(0);
+                        response.addCookie(returnLoginPassword);
+                        out.println("<BR><BR>You did not choose to remember the login information.Hence you will need to enter login details the next time you visit us.");
+                    }
+                }
+            }
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        /* TODO output your page here. You may use following sample code. */
+        out.println("<!DOCTYPE html>");
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Servlet RememberMeServlet</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1>Servlet RememberMeServlet at " + request.getContextPath() + "</h1>");
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
+
