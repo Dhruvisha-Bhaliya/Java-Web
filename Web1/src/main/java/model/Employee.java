e8ecd35feea1f5bd7ee6080ec00bc78fd9af4573
Dhruvisha <dhruvibhaliya01@gmail.com>
2025-09-07 22:20:30 +0530

    Add Fist Web Project

---
+++ Web1/src/main/java/model/Employee.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author DELL
+ */
+public class Employee {
+       
+    int empno;
+    String ename;
+    double salary;
+
+    public int getEmpno() {
+        return empno;
+    }
+
+    public void setEmpno(int empno) {
+        this.empno = empno;
+    }
+
+    public String getEname() {
+        return ename;
+    }
+
+    public void setEname(String ename) {
+        this.ename = ename;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+    
+    
+}
